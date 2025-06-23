705870cfdd18a93b88143c5218d9c2956632a9de
carlosreyes241 <106132197+carlosreyes241@users.noreply.github.com>
2025-06-23 01:53:19 -0600

    Update DbConnection.cs

--- DbConnections/DbConnection.cs
+++ DbConnections/DbConnection.cs
@@ -11,7 +11,7 @@ namespace prueba_tecnica_agroexport.DbConnections
     {
         public async Task<SqlConnection> Connection()
         {
-            string connectionString = "Data Source=DESKTOP-A30C9EP\\SQLEXPRESS;Initial Catalog=MublesAgroexport;Trusted_Connection=true; TrustServerCertificate=true;";
+            string connectionString = "Data Source=...SQLEXPRESS;Initial Catalog=MublesAgroexport;Trusted_Connection=true; TrustServerCertificate=true;";
             var connection = new SqlConnection(connectionString);
             await connection.OpenAsync();
             return connection;
